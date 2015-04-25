3027594e02b8f07b2abfee53b6e7c51139d83639
jlchereau <jlchereau@msn.com>
2015-04-25 09:15:53 +0100

    Centralized values reported from request

--- webapp/lib/logger.js
+++ webapp/lib/logger.js
@@ -12,26 +12,47 @@
 // https://github.com/logentries/le_node
 // https://logentries.com/doc/nodejs/
 
-var logentries = require('le_node'),
-    config = require('../config'),
+var config = require('../config'),
+    utils = require('./utils'),
+    logentries = require('le_node'),
     logger = logentries.logger({ token: config.get('logentries:server:token')});
+
+/**
+ * Process entry.request if existing
+ * @param entry
+ * @returns {*}
+ */
+function process(entry) {
+    if(entry.request) {
+        var request = entry.request;
+        entry = utils.deepExtend(entry, {
+            ip: request.ip,
+            url: request.url,
+            query: request.query,
+            agent: request.headers['user-agent']
+        });
+        delete entry.request;
+    }
+    return entry;
+}
+
 
 module.exports = {
 
     info: function(entry) {
-        logger.log('info', entry);
+        logger.log('info', process(entry));
     },
 
     warning: function(entry) {
-        logger.log('warning', entry);
+        logger.log('warning', process(entry));
     },
 
     error: function(entry) {
-        logger.log('err', entry);
+        logger.log('err', process(entry));
     },
 
     critical: function(entry) {
-        logger.log('crit', entry);
+        logger.log('crit', process(entry));
     },
 
     close: function() {
