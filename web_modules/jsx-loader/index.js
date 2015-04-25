bd0d2a47f235ef02ba8aff6a6a3bea4ab1974afd
jlchereau <jlchereau@msn.com>
2015-04-25 16:18:07 +0100

    Minor changes

--- web_modules/jsx-loader/index.js
+++ web_modules/jsx-loader/index.js
@@ -3,68 +3,8 @@ var webpack = require('webpack');
     ejs = require('ejs'),
     path = require('path'),
     fs = require('fs'),
-    async = require('async');
-
-//Copied from https://github.com/unclechu/node-deep-extend/blob/master/index.js
-function deepExtend(/*obj_1, [obj_2], [obj_N]*/) {
-    if (arguments.length < 1 || typeof arguments[0] !== 'object') {
-        return false;
-    }
-
-    if (arguments.length < 2) return arguments[0];
-
-    var target = arguments[0];
-
-    // convert arguments to array and cut off target object
-    var args = Array.prototype.slice.call(arguments, 1);
-
-    var key, val, src, clone, tmpBuf;
-
-    args.forEach(function (obj) {
-        if (typeof obj !== 'object') return;
-
-        for (key in obj) {
-            if ( ! (key in obj)) continue;
-
-            src = target[key];
-            val = obj[key];
-
-            if (val === target) continue;
-
-            if (typeof val !== 'object' || val === null) {
-                target[key] = val;
-                continue;
-            } else if (val instanceof Buffer) {
-                tmpBuf = new Buffer(val.length);
-                val.copy(tmpBuf);
-                target[key] = tmpBuf;
-                continue;
-            } else if (val instanceof Date) {
-                target[key] = new Date(val.getTime());
-                continue;
-            } else if (val instanceof RegExp) {
-                target[key] = new RegExp(val);
-                continue;
-            }
-
-            if (typeof src !== 'object' || src === null) {
-                clone = (Array.isArray(val)) ? [] : {};
-                target[key] = deepExtend(clone, val);
-                continue;
-            }
-
-            if (Array.isArray(val)) {
-                clone = (Array.isArray(src)) ? src : [];
-            } else {
-                clone = (!Array.isArray(src)) ? src : {};
-            }
-
-            target[key] = deepExtend(clone, val);
-        }
-    });
-
-    return target;
-}
+    async = require('async'),
+    deepExtend = require('deep-extend');
 
 module.exports = function(source) {
     var opts = loaderUtils.parseQuery(this.query),
@@ -81,21 +21,37 @@ module.exports = function(source) {
     async.parallel([
             function(done){
                 fs.readFile(defaultFile, 'utf-8', function(err, content) {
-                    if(err) return done(err);
-                    done(null, JSON.parse(content));
+                    if(err) {
+                        return done(err);
+                    } else {
+                        try {
+                            var data = JSON.parse(content);
+                            done(null, data);
+                        } catch (exception) {
+                            done(exception);
+                        }
+                    }
                 });
             },
             function(done){
                 fs.readFile(configFile, 'utf-8', function(err, content) {
-                    if(err) return done(err);
-                    done(null, JSON.parse(content));
+                    if(err) {
+                        return done(err);
+                    } else {
+                        try {
+                            var data = JSON.parse(content);
+                            done(null, data);
+                        } catch (exception) {
+                            done(exception);
+                        }
+                    }
                 });
             }
         ],
         function(err, result) {
             if(err) return callback(err);
-            var config = deepExtend(result[0], result[1]),
-                output = ejs.render(source, config);
+            var config = deepExtend(result[0], result[1]);
+            var output = ejs.render(source, config);
             callback(null, output);
         }
     );
