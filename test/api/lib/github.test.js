29f9d2e3a0b3947d4e54eac8c37d6940d8142e6c
jlchereau <jlchereau@msn.com>
2015-04-29 18:53:00 +0100

    Tested github lib

---
+++ test/api/lib/github.test.js
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) 2013-2015 Memba Sarl. All rights reserved.
+ * Sources at https://github.com/Memba
+ */
+
+/* jshint node: true, expr: true */
+/* globals describe: false, before: false, it: false */
+
+'use strict';
+
+var expect = require('chai').expect,
+    github = require('../../../webapp/lib/github');
+
+xdescribe('lib/github', function() {
+
+    var content = {
+        path: 'temp/dummy.md',
+        markdown: '# This is a dummy sample\n\nwith dummy content',
+        update: '# This is a dummy sample\n\nwith an update'
+    };
+
+    it('it should create content', function(done) {
+        github.createContent(content.path, content.markdown, function(error, response) {
+            expect(error).to.be.null;
+            expect(response).to.have.deep.property('commit.committer.name', 'membabot');
+            expect(response).to.have.deep.property('commit.message', 'System creation');
+            expect(response).to.have.deep.property('content.path', content.path);
+            expect(response).to.have.deep.property('content.html_url');
+            expect(response).to.have.deep.property('content.sha');
+            content.sha = response.content.sha;
+            done();
+        });
+    });
+
+    it('it should get commits', function(done) {
+        github.getCommits(content.path, function(error, response) {
+            expect(error).to.be.null;
+            expect(response).to.be.instanceof(Array);
+            done();
+        });
+    });
+
+    it('it should get content', function(done) {
+        github.getContent(content.path, function(error, response) {
+            expect(error).to.be.null;
+            expect(response).to.have.property('path', content.path);
+            expect(response).to.have.property('html_url');
+            expect(response).to.have.property('sha', content.sha);
+            done();
+        });
+    });
+
+    it('it should update content', function(done) {
+        expect(content.sha).not.to.be.undefined;
+        github.updateContent(content.path, content.update, content.sha, function(error, response) {
+            expect(error).to.be.null;
+            expect(response).to.have.deep.property('commit.committer.name', 'membabot');
+            expect(response).to.have.deep.property('commit.message', 'System update');
+            expect(response).to.have.deep.property('content.path', content.path);
+            expect(response).to.have.deep.property('content.html_url');
+            expect(response).to.have.deep.property('content.sha');
+            //content.sha changes which each update
+            content.sha = response.content.sha;
+            done();
+        });
+    });
+
+    it('it should get commits', function(done) {
+        github.getCommits(content.path, function(error, response) {
+            expect(error).to.be.null;
+            expect(response).to.be.instanceof(Array);
+            done();
+        });
+    });
+
+    it('it should delete content', function(done) {
+        expect(content.sha).not.to.be.undefined;
+        github.deleteContent(content.path, content.sha, function(error, response) {
+            expect(error).to.be.null;
+            expect(response).to.have.deep.property('commit.committer.name', 'membabot');
+            expect(response).to.have.deep.property('commit.message', 'System deletion');
+            expect(response).to.have.property('content', null);
+            done();
+        });
+    });
+
+    it('it should fail getting deleted content', function(done) {
+        github.getContent(content.path, function(error, response) {
+            expect(response).to.be.undefined;
+            expect(error).to.be.instanceof(Error);
+            done();
+        });
+    });
+
+    //Note: Updating deleted contents works as restoring
+
+});
--- webapp/lib/github.js
+++ webapp/lib/github.js
@@ -3,52 +3,134 @@
  * Sources at https://github.com/Memba
  */
 
-/* jslint node: true */
 /* jshint node: true */
 
 'use strict';
 
-var octonode = require('octonode'),
+var request = require('request'),
     config = require('../config'),
+    version = require('../../package.json').version,
 
-    github = octonode.client({
-        username: process.env.USERNAME,
-        password: process.env.PASSWORD
-    }),
-    repository = github.repo(config.get('github:repository')),
+    github = 'https://api.github.com',
+    authentication = {
+        user: process.env.USERNAME,
+        pass: process.env.PASSWORD
+    },
+    headers = {
+        'user-agent': 'Memba.Blog/' + version
+    },
+    repo = config.get('github:repository'),
     branch = config.get('github:branch');
-
 
 module.exports = {
 
+    //TODO: Consider creating the webhook via API
+
     /**
      * Get content from github
-     * @param uri
+     * @see https://developer.github.com/v3/repos/contents/#get-contents
+     * @param path
      * @param callback
      */
-    getContent: function(uri, callback) {
-        repository.contents(uri, branch, function(error, response) {
-            if(!error && response) {
-                var buf = new Buffer(response.content, 'base64');
-                callback(null, buf.toString());
+    getContent: function(path, callback) {
+        var uri = github + '/repos/' + repo + '/contents/' + path,
+            qs = { ref: branch };
+        request({ method: 'GET', uri: uri, headers: headers, auth: authentication, json: true, qs: qs }, function(error, response, body) {
+            if(!error && response.statusCode === 200) {
+                callback(null, body); //json: true in request options ensures that body is parsed to return an object
             } else {
-                callback(error);
+                callback(error || new Error('Getting Github content returned status ' + response.statusCode + (body && body.message ? ': ' + body.message : '')));
+            }
+        });
+    },
+
+    /**
+     * Create content into github
+     * @see https://developer.github.com/v3/repos/contents/#create-a-file
+     * @param path
+     * @param content
+     * @param callback
+     */
+    createContent: function(path, content, callback) {
+        var uri = github + '/repos/' + repo + '/contents/' + path,
+            data = {
+                branch: branch,
+                content: new Buffer(content).toString('base64'),
+                message: 'System creation'
+            };
+        //Note content creation requires PUT instead of POST
+        request({ method: 'PUT', uri: uri, headers: headers, auth: authentication, json: true, body: data }, function(error, response, body) {
+            if(!error && response.statusCode === 201) {
+                callback(null, body);
+            } else {
+                callback(error || new Error('Creating Github content returned status ' + response.statusCode + (body && body.message ? ': ' + body.message : '')));
             }
         });
     },
 
     /**
      * Update content into github
-     * @param uri
+     * @see https://developer.github.com/v3/repos/contents/#update-a-file
+     * @param path
      * @param content
+     * @param sha
      * @param callback
      */
-    updateContent: function(uri, content, callback) {
-        repository.contents(uri, branch, function(error, response) {
-            if(!error && response) {
-                repository.updateContents(uri, 'bot update', content, response.sha, callback);
+    updateContent: function(path, content, sha, callback) {
+        var uri = github + '/repos/' + repo + '/contents/' + path,
+            data = {
+                branch: branch,
+                content: new Buffer(content).toString('base64'),
+                message: 'System update',
+                sha: sha
+            };
+        request({ method: 'PUT', uri: uri, headers: headers, auth: authentication, json: true, body: data }, function(error, response, body) {
+            if(!error && (response.statusCode === 200 || response.statusCode === 201)) {
+                //updating content returns 200, except when updating deleted content which return 201 (like creation)
+                callback(null, body);
             } else {
-                callback(error);
+                callback(error || new Error('Updating Github content returned status ' + response.statusCode + (body && body.message ? ': ' + body.message : '')));
+            }
+        });
+    },
+
+    /**
+     * Delete content from github
+     * @see https://developer.github.com/v3/repos/contents/#delete-a-file
+     * @param path
+     * @param sha
+     * @param callback
+     */
+    deleteContent: function(path, sha, callback) {
+        var uri = github + '/repos/' + repo + '/contents/' + path,
+            data = {
+                branch: branch,
+                message: 'System deletion',
+                sha: sha
+            };
+        request({ method: 'DELETE', uri: uri, headers: headers, auth: authentication, json: true, body: data }, function(error, response, body) {
+            if(!error && response.statusCode === 200) {
+                callback(null, body);
+            } else {
+                callback(error || new Error('Deleting Github content returned status ' + response.statusCode + (body && body.message ? ': ' + body.message : '')));
+            }
+        });
+    },
+
+    /**
+     * Gets commits (especially to find author and date)
+     * @see https://developer.github.com/v3/repos/commits/#list-commits-on-a-repository
+     * @param path
+     * @param callback
+     */
+    getCommits: function(path, callback) {
+        var uri = github + '/repos/' + repo + '/commits',
+            qs = { path: path, sha: branch };
+        request({ method: 'GET', uri: uri, headers: headers, auth: authentication, json: true, qs: qs }, function(error, response, body) {
+            if(!error && response.statusCode === 200) {
+                callback(null, body);
+            } else {
+                callback(error || new Error('Getting Github commits returned status ' + response.statusCode + (body && body.message ? ': ' + body.message : '')));
             }
         });
     }
