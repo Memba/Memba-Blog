39a06071e8c4482cf22b2b586a1fc1f46d12e077
jlchereau <jlchereau@msn.com>
2015-05-06 14:41:28 +0100

    Updated cache

--- test/api/lib/cache.test.js
+++ test/api/lib/cache.test.js
@@ -13,20 +13,50 @@ var expect = require('chai').expect,
 
 describe('lib/cache', function() {
 
-    it('cache value for an unknown key is undefined', function() {
-        expect(cache.get('unknown')).to.be.undefined;
+    describe('menu', function () {
+
+        it('cache value for an unknown locale is undefined', function () {
+            var locale = 'zz';
+            expect(cache.getMenu(locale)).to.be.undefined;
+        });
+
+        it('cache value can be set and retrieved', function () {
+            var locale = 'en', menu = [{text: 'Support', href: '/en/'}];
+            expect(cache.setMenu(locale, menu)).not.to.throw;
+            expect(cache.getMenu(locale)).to.eql(menu);
+        });
+
+        it('cache can be reset', function () {
+            var locale = 'fr', menu = [{text: 'Support', href: '/fr/'}];
+            expect(cache.setMenu(locale, menu)).not.to.throw;
+            expect(cache.getMenu(locale)).to.eql(menu);
+            expect(cache.reset()).not.to.throw;
+            expect(cache.getMenu(locale)).to.be.undefined;
+        });
+
     });
 
-    it('an object in cache can be retrieved', function() {
-        cache.set('test', 100);
-        expect(cache.get('test')).to.equal(100);
-    });
+    describe('index', function () {
 
-    it('cache can be reset', function() {
-        cache.set('more', 200);
-        expect(cache.get('more')).to.equal(200);
-        cache.reset();
-        expect(cache.get('more')).to.be.undefined;
+        it('cache value for an unknown locale is undefined', function () {
+            var locale = 'zz';
+            expect(cache.getIndex(locale)).to.be.undefined;
+        });
+
+        it('cache value can be set and retrieved', function () {
+            var locale = 'en', index = [{uuid:'6a1df850-edb5-11e4-9453-f7b3ae7ed145', title: 'Frequently Asked Questions'}];
+            expect(cache.setIndex(locale, index)).not.to.throw;
+            expect(cache.getIndex(locale)).to.eql(index);
+        });
+
+        it('cache can be reset', function () {
+            var locale = 'fr', index = [{uuid:'6f5576a0-f0bb-11e4-8033-797a7732d2df', title: 'Questions Fréquentes'}];
+            expect(cache.setIndex(locale, index)).not.to.throw;
+            expect(cache.getIndex(locale)).to.eql(index);
+            expect(cache.reset()).not.to.throw;
+            expect(cache.getIndex(locale)).to.be.undefined;
+        });
+
     });
 
 });
