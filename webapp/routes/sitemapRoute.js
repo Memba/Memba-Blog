23a39c03060545bd1055745828256b533b5bd80b
jlchereau <jlchereau@msn.com>
2015-05-06 11:53:48 +0100

    Minor change

--- webapp/routes/sitemapRoute.js
+++ webapp/routes/sitemapRoute.js
@@ -40,7 +40,7 @@ module.exports = {
                 if(!error && Array.isArray(indexEntries)) {
                     var sitemap = '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
                     for(var i = 0; i < indexEntries.length; i++) {
-                        sitemap+= util.format('<url><loc>%s</loc></url>', indexEntries[i].site_url);
+                        sitemap += util.format('<url><loc>%s</loc></url>', indexEntries[i].site_url);
                     }
                     sitemap+= '</urlset>';
                     res
