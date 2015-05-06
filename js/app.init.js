c2098ef7e760e63a8a5a71d3c06615b6ed7521f5
jlchereau <jlchereau@msn.com>
2015-05-06 15:09:26 +0100

    Fixed jshint issue

--- js/app.init.js
+++ js/app.init.js
@@ -3,8 +3,8 @@
  * Sources at https://github.com/Memba
  */
 
-/* jshint browser: true, jquery: true */
-/* globals require: false */
+/* jshint browser: true */
+/* globals require: false, process: false */
 
 require('./app.config.jsx?env=' + process.env.NODE_ENV);
 require('./app.logger.js');
