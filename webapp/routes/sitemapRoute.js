563536c28ec62beb5843f4be5bf129f17fc20a17
jlchereau <jlchereau@msn.com>
2015-05-06 08:02:56 +0100

    Implemented sitemapRoute

---
+++ webapp/routes/sitemapRoute.js
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2013-2015 Memba Sarl. All rights reserved.
+ * Sources at https://github.com/Memba
+ */
+
+/* jshint node: true */
+
+'use strict';
+
+var util = require('util'),
+    config = require('../config'),
+    logger = require('../lib/logger'),
+    index = require('../models/indexModel');
+
+module.exports = {
+
+    /**
+     * Return a localized sitemap
+     * @see http://www.sitemaps.org/protocol.html
+     * @see https://support.google.com/webmasters/answer/156184?hl=en
+     * @param req
+     * @param res
+     * @param next
+     */
+    getMap: function(req, res, next) {
+        try {
+
+            //Create a sessionId that we can track in the browser
+            //req.sessionId = utils.uuid();
+
+            //Log the request
+            logger.info({
+                message: 'requesting a sitemap',
+                module: 'routes/sitemapRoute',
+                method: 'getMap',
+                request: req
+            });
+
+            index.getIndex(req.params.language, function(error, indexEntries) {
+                if(!error && Array.isArray(indexEntries)) {
+                    var sitemap = '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+                    for(var i = 0; i < indexEntries.length; i++) {
+                        sitemap+= util.format('<url><loc>%s</loc></url>', indexEntries[i].site_url);
+                    }
+                    sitemap+= '</urlset>';
+                    res
+                        .set({
+                            'Content-Type': 'application/xml; charset=utf-8',
+                            'Content-Language': res.getLocale()
+                        })
+                        .vary('Accept-Encoding') //See http://blog.maxcdn.com/accept-encoding-its-vary-important/
+                        .send(sitemap);
+                } else {
+                    next(error);
+                }
+            });
+
+        } catch (exception) {
+            next(exception);
+        }
+    }
+
+};
