d6e0fcfc15577beaf15502cc3372fbde8eae085b
jlchereau <jlchereau@msn.com>
2015-05-06 07:56:35 +0100

    Minor changes

--- webapp/lib/url.js
+++ webapp/lib/url.js
@@ -1,7 +1,11 @@
 /**
- * Created by jlchereau on 01/05/2015.
+ * Copyright (c) 2013-2015 Memba Sarl. All rights reserved.
+ * Sources at https://github.com/Memba
  */
 
+/* jshint node: true */
+
+'use strict';
 
 function normalize (str) {
     return str
@@ -11,13 +15,12 @@ function normalize (str) {
         .replace(/\:\//g, '://');
 }
 
-
 module.exports = {
 
     /**
-     *
+     * Join url fragments
      * @see https://github.com/jfromaniello/url-join/blob/master/lib/url-join.js
-     * @returns {*}
+     * @returns {string}
      */
     join: function() {
         var joined = [].slice.call(arguments, 0).join('/');
