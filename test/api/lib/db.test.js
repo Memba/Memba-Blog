7fe6d47568d83e1655b4383465ab1ab6e7736913
jlchereau <jlchereau@msn.com>
2015-05-28 16:55:18 +0100

    Add tests https://github.com/Memba/Memba-Blog/issues/11

--- test/api/lib/db.test.js
+++ test/api/lib/db.test.js
@@ -9,6 +9,8 @@
 'use strict';
 
 var expect = require('chai').expect,
+    util = require('util'),
+    config = require('../../../webapp/config'),
     db = require('../../../webapp/lib/db');
 
 describe('lib/db', function() {
@@ -68,8 +70,8 @@ describe('lib/db', function() {
         });
 
         it('Retrieve by language and path', function(done) {
-            var PATH = 'en/pages/faqs.md';
-            db.en.find({ language: 'en', path: PATH }, function(error, indexEntries) {
+            var path = util.format(config.get('github:language'), 'en') + config.get('github:pages') + '/' + util.format(config.get('github:markdown'), 'faqs');
+            db.en.find({ language: 'en', path: path }, function(error, indexEntries) {
                 expect(error).to.be.null;
                 expect(indexEntries).to.be.instanceof(Array).with.length.gte(1);
                 for (var i = 0; i < indexEntries.length; i++) {
@@ -83,7 +85,7 @@ describe('lib/db', function() {
                     expect(indexEntries[i]).to.have.property('icon').that.is.a('string');
                     expect(indexEntries[i]).to.have.property('keywords').that.is.a('string');
                     expect(indexEntries[i]).to.have.property('language', 'en');
-                    expect(indexEntries[i]).to.have.property('path', PATH);
+                    expect(indexEntries[i]).to.have.property('path', path);
                     expect(indexEntries[i]).to.have.property('text').that.is.a('string');
                     expect(indexEntries[i]).to.have.property('site_url').that.is.a('string');
                     expect(indexEntries[i]).to.have.property('title').that.is.a('string');
@@ -95,8 +97,8 @@ describe('lib/db', function() {
         });
 
         it('Retrieve by language and path with $eq', function(done) {
-            var PATH = 'en/pages/faqs.md';
-            db.en.find({ language: { $eq: 'en' }, path: { $eq: PATH } }, function(error, indexEntries) {
+            var path = util.format(config.get('github:language'), 'en') + config.get('github:pages') + '/' + util.format(config.get('github:markdown'), 'faqs');
+            db.en.find({ language: { $eq: 'en' }, path: { $eq: path } }, function(error, indexEntries) {
                 expect(error).to.be.null;
                 expect(indexEntries).to.be.instanceof(Array).with.length.gte(1);
                 for (var i = 0; i < indexEntries.length; i++) {
@@ -110,7 +112,7 @@ describe('lib/db', function() {
                     expect(indexEntries[i]).to.have.property('icon').that.is.a('string');
                     expect(indexEntries[i]).to.have.property('keywords').that.is.a('string');
                     expect(indexEntries[i]).to.have.property('language', 'en');
-                    expect(indexEntries[i]).to.have.property('path', PATH);
+                    expect(indexEntries[i]).to.have.property('path', path);
                     expect(indexEntries[i]).to.have.property('site_url').that.is.a('string');
                     expect(indexEntries[i]).to.have.property('text').that.is.a('string');
                     expect(indexEntries[i]).to.have.property('title').that.is.a('string');
@@ -122,8 +124,8 @@ describe('lib/db', function() {
         });
 
         it('Retrieve by category', function(done) {
-            var CATEGORY = 'Default';
-            db.en.find({ language: 'en', category: CATEGORY }, function(error, indexEntries) {
+            var category = 'Default';
+            db.en.find({ language: 'en', category: category }, function(error, indexEntries) {
                 expect(error).to.be.null;
                 expect(indexEntries).to.be.instanceof(Array).with.length.gte(1);
                 for (var i = 0; i < indexEntries.length; i++) {
@@ -131,7 +133,7 @@ describe('lib/db', function() {
                     expect(indexEntries[i]).to.have.property('author_url').that.is.a('string');
                     expect(indexEntries[i]).to.have.property('avatar_url').that.is.a('string');
                     expect(indexEntries[i]).to.have.property('creation_date').that.is.a('string');
-                    expect(indexEntries[i]).to.have.property('category', CATEGORY);
+                    expect(indexEntries[i]).to.have.property('category', category);
                     expect(indexEntries[i]).to.have.property('description').that.is.a('string');
                     expect(indexEntries[i]).to.have.property('edit_url').that.is.a('string');
                     expect(indexEntries[i]).to.have.property('icon').that.is.a('string');
@@ -149,12 +151,12 @@ describe('lib/db', function() {
         });
 
         it('Retrieve by author', function(done) {
-            var AUTHOR = 'Jacques L. Chereau';
-            db.en.find({ language: 'en', author: AUTHOR }, function(error, indexEntries) {
+            var author = 'jlchereau';
+            db.en.find({ language: 'en', author: author }, function(error, indexEntries) {
                 expect(error).to.be.null;
                 expect(indexEntries).to.be.instanceof(Array).with.length.gte(1);
                 for (var i = 0; i < indexEntries.length; i++) {
-                    expect(indexEntries[i]).to.have.property('author', AUTHOR);
+                    expect(indexEntries[i]).to.have.property('author', author);
                     expect(indexEntries[i]).to.have.property('author_url').that.is.a('string');
                     expect(indexEntries[i]).to.have.property('avatar_url').that.is.a('string');
                     expect(indexEntries[i]).to.have.property('creation_date').that.is.a('string');
@@ -176,8 +178,8 @@ describe('lib/db', function() {
         });
 
         it('Retrieve by root site_url', function(done) {
-            var SITE_URL = new RegExp('^http://localhost:3000/en/posts/2015/');
-            db.en.find({ language: 'en', site_url: SITE_URL }, function(error, indexEntries) {
+            var site_url = new RegExp('^' + config.get('uris:webapp:root') + util.format(config.get('uris:webapp:posts'), 'en', '2015', '', '').replace(/[\/]+$/, '/'));
+            db.en.find({ language: 'en', site_url: site_url }, function(error, indexEntries) {
                 expect(error).to.be.null;
                 expect(indexEntries).to.be.instanceof(Array).with.length.gte(1);
                 for (var i = 0; i < indexEntries.length; i++) {
@@ -192,7 +194,7 @@ describe('lib/db', function() {
                     expect(indexEntries[i]).to.have.property('keywords').that.is.a('string');
                     expect(indexEntries[i]).to.have.property('language', 'en');
                     expect(indexEntries[i]).to.have.property('path').that.is.a('string');
-                    expect(indexEntries[i]).to.have.property('site_url').that.match(SITE_URL);
+                    expect(indexEntries[i]).to.have.property('site_url').that.match(site_url);
                     expect(indexEntries[i]).to.have.property('text').that.is.a('string');
                     expect(indexEntries[i]).to.have.property('title').that.is.a('string');
                     //expect(indexEntries[i]).to.have.property('update_date').that.is.a('string');
@@ -203,8 +205,8 @@ describe('lib/db', function() {
         });
 
         it('Retrieve by full text search', function(done) {
-            var TEXT = new RegExp('support');
-            db.en.find({ language: 'en', text: TEXT }, function(error, indexEntries) {
+            var text = new RegExp('support');
+            db.en.find({ language: 'en', text: text }, function(error, indexEntries) {
                 expect(error).to.be.null;
                 expect(indexEntries).to.be.instanceof(Array).with.length.gte(1);
                 for (var i = 0; i < indexEntries.length; i++) {
@@ -220,7 +222,7 @@ describe('lib/db', function() {
                     expect(indexEntries[i]).to.have.property('language', 'en');
                     expect(indexEntries[i]).to.have.property('path').that.is.a('string');
                     expect(indexEntries[i]).to.have.property('site_url').that.is.a('string');
-                    expect(indexEntries[i]).to.have.property('text').that.match(TEXT);
+                    expect(indexEntries[i]).to.have.property('text').that.match(text);
                     expect(indexEntries[i]).to.have.property('title').that.is.a('string');
                     //expect(indexEntries[i]).to.have.property('update_date').that.is.a('string');
                     expect(indexEntries[i]).to.have.property('uuid').that.is.a('string');
