0df0f42db013416edf2657aa2555dfc6267bf764
jlchereau <jlchereau@msn.com>
2015-06-09 17:49:58 +0100

    Copied from Kidoju.WepApp

---
+++ test/browsers/sample.js
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2013-2015 Memba Sarl. All rights reserved.
+ * Sources at https://github.com/Memba/Kidoju-Platform
+ */
+
+/* jslint browser: true */
+/* jshint browser: true */
+/* global describe, it, expect */
+
+;(function(window) {
+
+    'use strict';
+
+    var expect = window.chai.expect;
+
+    describe('Test PageItemCollectionDataSource', function() {
+
+        describe('When doing something', function() {
+            it('We expect something else', function() {
+                expect(true).to.equal(true);
+            });
+        });
+
+    });
+
+}(this));
