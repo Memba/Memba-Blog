7499ed36beba385d5f1e329e5132b2690ee4a6ea
jlchereau <jlchereau@msn.com>
2015-06-17 11:27:05 +0100

    Replaced marked by markdown-it https://github.com/Memba/Memba-Blog/issues/55

--- webapp/lib/markdown.js
+++ webapp/lib/markdown.js
@@ -7,67 +7,47 @@
 
 'use strict';
 
-var marked = require('marked'),
+var MarkdownIt = require('markdown-it'),
     hljs = require('highlight.js'),
-    renderer = new marked.Renderer();
+    markdown = new MarkdownIt({
+        html: true,
+        linkify: true,
+        typographer: true,
+        //See https://github.com/markdown-it/markdown-it#syntax-highlighting
+        highlight: function (code, lang) {
+            try {
+                return hljs.highlight(lang, code).value;
+            } catch(err) {
+                return hljs.highlightAuto(code).value;
+            }
+        }
+    });
 
 var RX_YML = /^---\n([\s\S]*)\n---/,
     RX_KEYVAL = /([^:\n]+):([^\n]+)/g,
     KEY_BLACKLIST = /[-\s]/g;
 
-//hack: we need a renderer to add proper pre code classes until markedjs catches up with highlighjs v0.8x
-//See https://github.com/chjj/marked/issues/336
-renderer.code = function(code, lang) {
-
-    var highlight = this.options.highlight,
-        html = highlight(code, lang);
-
-    return '<pre><code' +
-        (lang ? ' class="hljs ' + this.options.langPrefix + lang + '"' : ' class="hljs"') +
-        '>' +
-        html +
-        '\n</code></pre>\n';
-
-};
-
-marked.setOptions({
-    renderer: renderer,
-    gfm: true,
-    tables: true,
-    breaks: false,
-    pedantic: false,
-    sanitize: true,
-    smartLists: true,
-    smartypants: false,
-    highlight: function (code, lang) {
-        try {
-            return hljs.highlight(lang, code).value;
-        } catch(err) {
-            return hljs.highlightAuto(code).value;
-        }
-    }
-});
 
 module.exports = {
 
     /**
-     * Render markdown as HTML after removing yml
-     * Note: clean first
-     * @param markdown
+     * Render markdown as HTML
+     * Note: extract markdown body first -- see below
+     * @param body
      * @returns {*}
      */
-    render: function(markdown) {
-        return marked(markdown);
+    render: function(body) {
+        return markdown.render(body);
     },
 
     /**
-     * Extract key/value pairs from yml at the top of markdown
-     * @param markdown
+     * Extract key/value pairs from yml section at the top of content
+     * @param content
      * @returns {{title: string, description: string}}
      */
-    head: function(markdown) {
+    head: function(content) {
         var yml = {},
-            ymlMatches = markdown.match(RX_YML);
+            ymlMatches = content.match(RX_YML);
         if (Array.isArray(ymlMatches) && ymlMatches.length > 1) {
             var keyvalMatches = ymlMatches[1].match(RX_KEYVAL);
             if (Array.isArray(keyvalMatches) && keyvalMatches.length) {
@@ -84,12 +64,12 @@ module.exports = {
     },
 
     /**
-     * Clean markdown from yml
-     * @param markdown
+     * Remove yml section from content
+     * @param content
      * @returns {*}
      */
-    body: function(markdown) {
-        return markdown.replace(RX_YML, '').trim();
+    body: function(content) {
+        return content.replace(RX_YML, '').trim();
     }
 
 };
