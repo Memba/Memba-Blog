51710b1b74fe8bdf7d12e773fa6a072294a15745
jlchereau <jlchereau@msn.com>
2015-06-24 11:43:35 +0100

    Updated to include kendo mobile styles

--- styles/vendor/kendo/update.cmd
+++ styles/vendor/kendo/update.cmd
@@ -7,3 +7,4 @@ del /S /Q web\*.css
 REM use -i.bak if you need a backup file before changes
 sed -i 's/fonts\/DejaVu\/Deja/Deja/g' web/fonts/DejaVu/dejavu.less
 sed -i 's/"textures/"..\/textures/g' web/common/inputs.less
+sed -i 's/"images/"..\/images/g' mobile/common/icons.less
