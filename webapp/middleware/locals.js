548987e597a800106ece641d2a906ecd724bab2e
jlchereau <jlchereau@msn.com>
2015-11-28 18:51:31 +0000

    Restored moment

--- webapp/middleware/locals.js
+++ webapp/middleware/locals.js
@@ -8,41 +8,15 @@
 
 'use strict';
 
-var util = require('util'),
-    // moment = require('moment'),
-    config = require('../config'),
-    url = require('../lib/url');
+var util = require('util');
+var moment = require('moment');
+var config = require('../config');
+var url = require('../lib/url');
 
 module.exports = function (req, res, next) {
 
     // Make nconf configurations available to our EJS templates
     res.locals.config = config.get();
-
-    /*
-    res.locals.config = {
-        debug               : config.get('debug'),
-        locales                 : config.get('locales'),
-        logentries: {
-            browser: {
-                token           : config.get('logentries:browser:token')
-            }
-        },
-        uris : {
-            webapp: {
-                editor          : url.join(config.get('uris:webapp:root'), config.get('uris:webapp:editor')),
-                finder          : url.join(config.get('uris:webapp:root'), config.get('uris:webapp:finder')),
-                home            : url.join(config.get('uris:webapp:root'), config.get('uris:webapp:home')),
-                ping            : url.join(config.get('uris:webapp:root'), config.get('uris:webapp:ping')),
-                player          : url.join(config.get('uris:webapp:root'), config.get('uris:webapp:player')),
-                public          : url.join(config.get('uris:webapp:root'), config.get('uris:webapp:public')),
-                rss             : url.join(config.get('uris:webapp:root'), config.get('uris:webapp:rss')),
-                sitemap         : url.join(config.get('uris:webapp:root'), config.get('uris:webapp:sitemap')),
-                summary         : url.join(config.get('uris:webapp:root'), config.get('uris:webapp:summary')),
-                user            : url.join(config.get('uris:webapp:root'), config.get('uris:webapp:user'))
-            }
-        }
-    };
-    */
 
     // format function
     res.locals.format = function () {
@@ -55,7 +29,7 @@ module.exports = function (req, res, next) {
     };
 
     // moment
-    // res.locals.moment = moment;
+    res.locals.moment = moment;
 
     // Pass the req to the next middleware
     next();
