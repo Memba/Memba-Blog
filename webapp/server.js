e86093a1c07df744f8d34652257e589f6495b2a6
jlchereau <jlchereau@msn.com>
2016-01-21 11:30:24 +0000

    Updated from Kidoju-WebApp

--- webapp/server.js
+++ webapp/server.js
@@ -14,18 +14,16 @@ var logger = require('./lib/logger');
  * Handle uncaught exceptions
  * @see http://stackoverflow.com/questions/7310521/node-js-best-practice-exception-handling
  */
-process.on('uncaughtException', function (ex) {
+process.on('uncaughtException', function (exception) {
 
     logger.critical({
         // message = exception.message
         module: 'server',
         method: 'process.onuncaughtException',
-        error: ex
+        error: exception
     });
 
-    if (typeof server === 'undefined') {
-        process.exit(1);
-    } else {
+    if (typeof server !== 'undefined') {
         // make sure we close down within 2 seconds
         // which should give enough time for any pending request to complete
         var killtimer = setTimeout(function () {
@@ -35,6 +33,8 @@ process.on('uncaughtException', function (ex) {
         killtimer.unref();
         // stop taking new requests
         server.close();
+    } else {
+        process.exit(1);
     }
 
 });
@@ -60,62 +60,74 @@ process.on('exit', function (code) {
             method: 'process.onexit'
         });
     } catch (ex) {
+        // We use console.log because logger might not be available
         console.error(ex);
     }
 });
 
-var config = require('./config');
-
-logger.info({
-    message: 'nconf environment is ' + config.get('NODE:ENV'),
-    module: 'config/index'
-});
-
-var path = require('path');
 var express = require('express');
+var app = express();
 var helmet = require('helmet');
 var http = require('http');
 var i18n = require('i18n');
-var router = require('./routes');
-var app = express();
-var port = process.env.PORT || config.get('express:port');
+var path = require('path');
+var config = require('./config');
+var router;
+var port;
+var server;
 
-// Secure expressJS with helmet from https://github.com/helmetjs/helmet
-// app.disable('x-powered-by');
-app.use(helmet());
-
-// Configure expressJS
-app.set('trust proxy', 'uniquelocal');
-app.set('port', port);
-
-// i18n
-i18n.configure({
-    locales: config.get('locales'), // ['en', 'fr'],
-    directory: path.join(__dirname, 'locales'),
-    objectNotation: true // Use hierarchies in locales.json files
-});
-// Use __() in templates
-app.use(i18n.init);
-
-// Template engine
-app.engine('.ejs', require('ejs').__express);
-// Optional since express defaults to CWD/views
-app.set('views', path.join(__dirname, 'views'));
-// Without this one would need to supply the extension to res.render(), ex: res.render('users.html').
-app.set('view engine', 'ejs');
-
-// Static files (before routing)
-// Cache-Control maxAge requires a string in MS format - see https://www.npmjs.com/package/ms
-app.use(util.format(config.get('uris:webapp:public'), ''), express.static(path.join(__dirname, 'public'), { maxAge: '1d' }));
-
-// Routing
-app.use(router);
-
-// Launch server
-var server = http.createServer(app).listen(port);
 logger.info({
-    message: 'expressJS server listening on port ' + port,
+    message: 'nconf environment is ' + config.environment,
     module: 'server'
+});
+
+config.load(function (error/*, store*/) {
+
+    if (error instanceof Error) {
+        throw error;
+    }
+
+    // Secure expressJS with helmet from https://github.com/helmetjs/helmet
+    // app.disable('x-powered-by');
+    app.use(helmet());
+
+    // i18n
+    i18n.configure({
+        locales: config.get('locales'), // ['en', 'fr'],
+        directory: path.join(__dirname, 'locales'),
+        objectNotation: true // Use hierarchies in locales.json files
+    });
+    // Use __() in templates
+    app.use(i18n.init);
+
+    // Template engine
+    app.engine('.ejs', require('ejs').__express);
+    // Optional since express defaults to CWD/views
+    app.set('views', path.join(__dirname, 'views'));
+    // Without this one would need to supply the extension to res.render(), ex: res.render('users.html').
+    app.set('view engine', 'ejs');
+
+    // Static files (before routing)
+    // Cache-Control maxAge requires a string in MS format - see https://www.npmjs.com/package/ms
+    app.use(util.format(config.get('uris:webapp:public'), ''), express.static(path.join(__dirname, 'public'), {maxAge: '1d'}));
+
+    // Routing
+    router = require('./routes');
+    app.use(router);
+
+    // Configure expressJS and launch server
+    port = process.env.PORT || config.get('express:port');
+    app.set('port', port);
+    app.set('trust proxy', 'uniquelocal');
+    server = http.createServer(app).listen(port);
+
+    // Logging
+    logger.info({
+        message: 'expressJS server listening on port ' + port,
+        module: 'server',
+        method: 'config.load'
+    });
+
 });
 
 // Export app for unit tests
