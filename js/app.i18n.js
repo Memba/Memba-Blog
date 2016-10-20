553fdc48a107e3ee8bc832db6563d04a2789c86b
jlchereau <jlchereau@msn.com>
2016-10-20 20:49:20 +0100

    Copied from Kidoju-WebApp

--- js/app.i18n.js
+++ js/app.i18n.js
@@ -115,17 +115,24 @@
          * Load page locale (read from html tag)
          */
         $(function () {
+            function fireLoaded() {
+                // Log readiness
+                logger.debug({
+                    message: locale + ' locale loaded',
+                    method: 'document.ready'
+                });
+                // trigger event for client localization of page
+                $(document).trigger(LOADED);
+            }
             var locale = i18n.locale();
             i18n.load(locale)
                 .then(function () {
-                    // Log readiness
-                    logger.debug({
-                        message: locale + ' locale loaded',
-                        method: 'document.ready'
-                    });
-
-                    // trigger event for client localization of page
-                    $(document).trigger(LOADED);
+                    if ($.type(window.cordova) === UNDEFINED) {
+                        fireLoaded()
+                    } else {
+                        // only fire after device is ready
+                        document.addEventListener('deviceready', fireLoaded, false);
+                    }
                 });
         });
 
