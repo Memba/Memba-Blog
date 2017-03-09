b0d667b8aa7c71ca22c6b39540c6ac59a1e1f3d6
jlchereau <jlchereau@msn.com>
2017-03-09 15:56:32 +0000

    Copied from Kidoju-Server

--- webapp/routes/loggerRoute.js
+++ webapp/routes/loggerRoute.js
@@ -26,23 +26,34 @@ module.exports = {
      */
     createEntry: function (req, res, next) {
 
-        // Assert body (after being parsed by body-parser)
-        var body = req.body;
+        try {
 
-        if (utils.isObject(body) &&
-            typeof body.date === 'string' &&
-            RX_LEVEL.test(body.level) &&
-            typeof body.message === 'string') { // We could also check that we have either (i) module + method, or (ii) error stack
+            // Assert body (after being parsed by body-parser)
+            var body = req.body;
 
-            // Read the trace from headers
-            req.trace = req.headers['x-trace-id'];
+            if (utils.isObject(body) &&
+                typeof body.date === 'string' &&
+                RX_LEVEL.test(body.level) &&
+                typeof body.message === 'string') { // We could also check that we have either (i) module + method, or (ii) error stack
 
-            // Log the request
-            body.request = req;
-            logger[body.level.toLowerCase()](body);
+                // Read the trace from headers
+                req.trace = req.headers['x-trace-id'];
+
+                // Log the request
+                body.request = req;
+                logger[body.level.toLowerCase()](body);
+            }
+
+            // Return ok in all circumstances
+            res.status(httpStatus.created).end();
+
+        } catch (ex) {
+
+            // Any error here should be caught to avoid a critical error that restarts the nodejs process
+            // See https://github.com/jlchereau/Kidoju-Server/issues/150
+            next(ex);
+
         }
 
-        // Return ok in all circumstances
-        res.status(httpStatus.created).end();
     }
 };
