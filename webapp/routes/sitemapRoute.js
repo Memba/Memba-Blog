8bcc49eb6c729fa746df19313e8fad82fa4bde2c
jlchereau <jlchereau@msn.com>
2018-12-20 14:10:33 +0000

    ES6 Code Migration (./webapp/routes/sitemapRoute.es6)

--- webapp/routes/sitemapRoute.js
+++ webapp/routes/sitemapRoute.js
@@ -3,31 +3,26 @@
  * Sources at https://github.com/Memba
  */
 
-/* jshint node: true */
+const assert = require('assert');
+const logger = require('../lib/logger.es6');
+const indexModel = require('../models/indexModel.es6');
 
-'use strict';
-
-var assert = require('assert');
-var url = require('url');
-var util = require('util');
-var config = require('../config/index.es6');
-var logger = require('../lib/logger.es6');
-var indexModel = require('../models/indexModel');
-
-var XML_HEAD = '<?xml version="1.0" encoding="UTF-8"?>';
+const XML_HEAD = '<?xml version="1.0" encoding="UTF-8"?>';
 
 // Sitemap indexes
-var INDEX_BEGIN = '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/siteindex.xsd">';
-var INDEX_ITEM = '<sitemap><loc>%s</loc></sitemap>';
-var INDEX_END = '</sitemapindex>';
+const INDEX_BEGIN =
+    '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/siteindex.xsd">';
+const INDEX_ITEM = '<sitemap><loc>%s</loc></sitemap>';
+const INDEX_END = '</sitemapindex>';
 
 // Sitemap
-var SITEMAP_BEGIN = '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">';
-var SITEMAP_ITEM = '<url><loc>%s</loc><lastmod>%s</lastmod><changefreq>%s</changefreq><priority>%s</priority></url>';
-var SITEMAP_END = '</urlset>';
+const SITEMAP_BEGIN =
+    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">';
+const SITEMAP_ITEM =
+    '<url><loc>%s</loc><lastmod>%s</lastmod><changefreq>%s</changefreq><priority>%s</priority></url>';
+const SITEMAP_END = '</urlset>';
 
 module.exports = {
-
     /**
      * Return a localized sitemap
      * @see http://www.sitemaps.org/protocol.html
@@ -36,11 +31,8 @@ module.exports = {
      * @param res
      * @param next
      */
-    getXmlSitemap: function (req, res, next) {
-
-        // var config = res.locals.config;
-        var format = res.locals.format;
-        // var url = res.locals.url;
+    getXmlSitemap(req, res, next) {
+        const { config, format, URL } = res.locals;
 
         // Create a trace that we can track in the browser
         // req.trace = utils.uuid();
@@ -53,52 +45,61 @@ module.exports = {
             request: req
         });
 
-        var language = req.params.language;
+        const { language } = req.params;
+        assert.strictEqual(
+            language,
+            res.getLocale(),
+            format('i18n locale is not `{0}`', language)
+        );
 
         if (language) {
-
-            indexModel.getIndex(req.params.language, function (error, indexEntries) {
+            indexModel.getIndex(req.params.language, (error, indexEntries) => {
                 if (!error && Array.isArray(indexEntries)) {
+                    let sitemap = XML_HEAD + SITEMAP_BEGIN;
 
-                    var sitemap = XML_HEAD + SITEMAP_BEGIN;
-
-                    for (var i = 0; i < indexEntries.length; i++) {
+                    for (let i = 0; i < indexEntries.length; i++) {
                         /* jscs: disable requireCamelCaseOrUpperCaseIdentifiers */
-                        var loc = indexEntries[i].site_url;
-                        var lastmod = indexEntries[i].creation_date;
-                        var changefreq = 'monthly';
-                        var priority = '1.0';
+                        const loc = indexEntries[i].site_url;
+                        const lastmod = indexEntries[i].creation_date;
+                        const changefreq = 'monthly';
+                        const priority = '1.0';
                         /* jscs: enable requireCamelCaseOrUpperCaseIdentifiers */
-                        sitemap += util.format(SITEMAP_ITEM, loc, lastmod, changefreq, priority);
+                        sitemap += format(
+                            SITEMAP_ITEM,
+                            loc,
+                            lastmod,
+                            changefreq,
+                            priority
+                        );
                     }
 
                     sitemap += SITEMAP_END;
 
-                    res
-                        .set({
-                            'Cache-Control': 'private, max-age=43200',
-                            'Content-Language' : language,
-                            'Content-Type': 'application/xml; charset=utf-8'
-                        })
-                        .vary('Accept-Encoding') // See http://blog.maxcdn.com/accept-encoding-its-vary-important/
+                    res.set({
+                        'Cache-Control': 'private, max-age=43200',
+                        'Content-Language': language,
+                        'Content-Type': 'application/xml; charset=utf-8'
+                    })
+                    .vary('Accept-Encoding') // See http://blog.maxcdn.com/accept-encoding-its-vary-important/
                         .send(sitemap);
                 } else {
                     next(error);
                 }
             });
-
         } else {
-
-            var locales = res.locals.getLocales();
+            const locales = res.locals.getLocales();
             // TODO compare with config?
 
-            var index = XML_HEAD + INDEX_BEGIN;
+            let index = XML_HEAD + INDEX_BEGIN;
 
             if (Array.isArray(locales)) {
                 // Iterate over locales
-                for (var i = 0; i < locales.length; i++) {
-                    var loc = url.resolve(config.get('uris:webapp:root'), util.format(config.get('uris:webapp:sitemap'), locales[i]));
-                    index += util.format(INDEX_ITEM, loc);
+                for (let i = 0; i < locales.length; i++) {
+                    const loc = new URL(
+                        format(config.get('uris:webapp:sitemap'), locales[i]),
+                        config.get('uris:webapp:root')
+                    ).href;
+                    index += format(INDEX_ITEM, loc);
                 }
             }
 
@@ -106,14 +107,11 @@ module.exports = {
 
             // Send the index
             res.set({
-                    'Content-Type': 'application/xml; charset=utf-8'
-                    // 'Cache-Control': 'max-age=3600, public'
-                })
-                .vary('Accept-Encoding') // See http://blog.maxcdn.com/accept-encoding-its-vary-important/
+                'Content-Type': 'application/xml; charset=utf-8'
+                // 'Cache-Control': 'max-age=3600, public'
+            })
+            .vary('Accept-Encoding') // See http://blog.maxcdn.com/accept-encoding-its-vary-important/
                 .send(index);
-
         }
-
     }
-
 };
