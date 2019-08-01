857f7f6fef11b3ac93d602db372fa7ca3d16fa25
jlchereau <jlchereau@msn.com>
2019-08-01 18:29:18 +0100

    New production build

---
+++ webapp/public/build/precache-manifest.b4f6f34aeaabcf63c608994c41898bcd.js
@@ -0,0 +1,103 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73e165b90423a62f835c45432ecb9dbc",
+    "url": "https://www.memba.com/build/../sw.js"
+  },
+  {
+    "url": "https://www.memba.com/build/06c0e4eee47149717c20a979ffb49b61.eot"
+  },
+  {
+    "url": "https://www.memba.com/build/9e0bdff4970e28b99fd7c45d700c20fa.jpg"
+  },
+  {
+    "revision": "a8888718e28a6fb02354",
+    "url": "https://www.memba.com/build/app-culture-en-es6.bundle.js?v=0.3.8"
+  },
+  {
+    "revision": "367f08b596a360f5d7b5",
+    "url": "https://www.memba.com/build/app-culture-fr-es6.bundle.js?v=0.3.8"
+  },
+  {
+    "revision": "9994228a67fef6111d41",
+    "url": "https://www.memba.com/build/app.theme.black.bundle.js?v=0.3.8"
+  },
+  {
+    "revision": "072962682b9b66dfde15",
+    "url": "https://www.memba.com/build/app.theme.bootstrap.bundle.js?v=0.3.8"
+  },
+  {
+    "revision": "1f605274326b087b8392",
+    "url": "https://www.memba.com/build/app.theme.flat.bundle.js?v=0.3.8"
+  },
+  {
+    "revision": "93cfa9e701d112790943",
+    "url": "https://www.memba.com/build/app.theme.highcontrast.bundle.js?v=0.3.8"
+  },
+  {
+    "revision": "53c12e7919e1f7b93b7a",
+    "url": "https://www.memba.com/build/app.theme.indigo.bundle.js?v=0.3.8"
+  },
+  {
+    "revision": "6293e52555586ff8ef05",
+    "url": "https://www.memba.com/build/app.theme.memba.bundle.js?v=0.3.8"
+  },
+  {
+    "revision": "a5c6d9e826b764c3224b",
+    "url": "https://www.memba.com/build/app.theme.nordic.bundle.js?v=0.3.8"
+  },
+  {
+    "revision": "95b9fd809b850fe36fd0",
+    "url": "https://www.memba.com/build/app.theme.turquoise.bundle.js?v=0.3.8"
+  },
+  {
+    "revision": "1a1c4ca9e86675bb78a8",
+    "url": "https://www.memba.com/build/app.theme.urban.bundle.js?v=0.3.8"
+  },
+  {
+    "revision": "12a06c8e4bbf4a7206fb",
+    "url": "https://www.memba.com/build/app.theme.vintage.bundle.js?v=0.3.8"
+  },
+  {
+    "url": "https://www.memba.com/build/cbb99b5861be0813c86d8b564e49a468.png"
+  },
+  {
+    "url": "https://www.memba.com/build/cdfd82cd7caa66cfe027d1702b434bd9.png"
+  },
+  {
+    "revision": "b0e0ee96f9d9adc54939",
+    "url": "https://www.memba.com/build/common.bundle.js?v=0.3.8"
+  },
+  {
+    "url": "https://www.memba.com/build/e811d21d46e69ff03a92761dd0b9f47e.woff"
+  },
+  {
+    "revision": "5d0b9d1e2dbb1c8515ef",
+    "url": "https://www.memba.com/build/error.bundle.js?v=0.3.8"
+  },
+  {
+    "url": "https://www.memba.com/build/f0e822cb280b38eaa00d74d242a837f9.ttf"
+  },
+  {
+    "revision": "0cb6ff2afea12e7fecca",
+    "url": "https://www.memba.com/build/home.bundle.js?v=0.3.8"
+  },
+  {
+    "revision": "e66f6f89887e62db7efb",
+    "url": "https://www.memba.com/build/init.bundle.js?v=0.3.8"
+  },
+  {
+    "revision": "4c18e441bccc6f037a84",
+    "url": "https://www.memba.com/build/page.bundle.js?v=0.3.8"
+  },
+  {
+    "revision": "f267eaa055c147136423",
+    "url": "https://www.memba.com/build/post.bundle.js?v=0.3.8"
+  },
+  {
+    "url": "https://www.memba.com/build/precache-manifest.2c7425c73d14ac19a1bf46387ee4c1c3.js"
+  },
+  {
+    "revision": "72f50e5ae24e42ca2fe1",
+    "url": "https://www.memba.com/build/search.bundle.js?v=0.3.8"
+  }
+]);
