ed2fdc288b8d65e667274e23e1c47295529a05f7
jlchereau <jlchereau@msn.com>
2022-11-05 18:57:04 +0100

    Upgraded Kendo UI from v2022.2.802 to v2022.3.913

--- src/js/helpers/system.config.js
+++ src/js/helpers/system.config.js
@@ -30,7 +30,7 @@ SystemJS.config({
         kendospreadsheet: ['kendo.spreadsheet'],
     },
     map: {
-        jquery: 'src/js/vendor/jquery/jquery-3.6.0.js',
+        jquery: 'src/js/vendor/jquery/jquery-3.6.1.js',
         // http://docs.telerik.com/kendo-ui/third-party/systemjs
         'kendo.culture.en-GB':
             'src/js/vendor/kendo/cultures/kendo.culture.en-GB.js',
@@ -185,6 +185,7 @@ SystemJS.config({
             'src/js/vendor/kendo/kendo.scheduler.yearview.js',
         'kendo.scrollview': 'src/js/vendor/kendo/kendo.scrollview.js',
         'kendo.selectable': 'src/js/vendor/kendo/kendo.selectable.js',
+        'kendo.signature': 'src/js/vendor/kendo/kendo.signature.js',
         'kendo.skeletoncontainer':
             'src/js/vendor/kendo/kendo.skeletoncontainer.js',
         'kendo.slider': 'src/js/vendor/kendo/kendo.slider.js',
